1a2afb0ded648fca202dfb3f523e52518864224b
Giulianna Mandolesi <gium.terra@gmail.com>
2018-01-17 00:28:11 -0200

    Basics - Part 2

---
+++ SQL_Basics/README.md
@@ -0,0 +1,9 @@
+1) groceries_insert_select.txt
+2) movies_orderby.txt
+3) todo_sum.txt
+4) groceries_update_groupby.txt
+5) exercise_logs_and_or.txt
+6) songs_and_or.txt
+7) exercise_logs_in.txt
+8) drs_favorite_exercise_logs_like.txt
+
--- SQL_Basics/grocery.py
+++ SQL_Basics/grocery.py
@@ -6,12 +6,12 @@ conn = sqlite3.connect('tutorial.db')
 c = conn.cursor()
 
 def create_table():
-	c.execute('CREATE TABLE IF NOT EXISTS groceries(id INTEGER PRIMARY KEY, name TEXT, quantity REAL)')
+	c.execute('CREATE TABLE IF NOT EXISTS groceries (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER, aisle INTEGER)')
 
 def data_entry():
-	c.execute('INSERT INTO groceries VALUES (1, "Bananas",4)')
-	c.execute('INSERT INTO groceries VALUES (2, "Peanut Butter", 1)')
-	c.execute('INSERT INTO groceries VALUES (3, "Dark chocolate bars", 2)')
+	c.execute('INSERT INTO groceries VALUES (1, "Bananas",4, 7)')
+	c.execute('INSERT INTO groceries VALUES (2, "Peanut Butter", 1, 2)')
+	c.execute('INSERT INTO groceries VALUES (3, "Dark chocolate bars", 2, 2)')
 	c.execute('SELECT * FROM groceries')
 
 	conn.commit()
